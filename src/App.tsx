100c0fe8a9495447dbfa4e40b838491ecf712c25
jtstrader <jacob.thomas.strader@gmail.com>
2025-12-02 21:29:54 -0500

    feat: Add frontend logic to call backend search

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,42 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { createSignal, onMount } from "solid-js";
+import { search } from "./commands";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  const [searchResults, setSearchResults] = createSignal([] as string[]);
+  const [key, setKey] = createSignal(null as string | null);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
+  onMount(async () => {
+    await updateKeyAndSearch("");
+  });
+
+  /**
+   * Update the search key and perform a search.
+   * @param newKey The new key to use during the search.
+   */
+  const updateKeyAndSearch = async (newKey: string) => {
+    let results = await search(newKey, key());
+    setKey(newKey);
+    setSearchResults(results);
+  };
 
   return (
     <main class="container">
-      <h1>Welcome to Tauri + Solid</h1>
+      <h1>lore</h1>
 
-      <div class="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+      <input
+        id="greet-input"
+        onChange={(e) => updateKeyAndSearch(e.currentTarget.value)}
+        onKeyPress={(e) => updateKeyAndSearch(e.currentTarget.value)}
+        onPaste={(e) => updateKeyAndSearch(e.currentTarget.value)}
+        onInput={(e) => updateKeyAndSearch(e.currentTarget.value)}
+        autoCapitalize="none"
+        autocomplete="off"
+        autocorrect="off"
+        spellcheck="false"
+      />
 
-      <form
-        class="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg()}</p>
+      <p>{searchResults().join(", ")}</p>
     </main>
   );
 }
---
+++ src/commands.ts
@@ -0,0 +1,15 @@
+import { invoke } from "@tauri-apps/api/core";
+
+/**
+ * Invoke the backend search command to look for items matching the provided key.
+ * @param key - The search key.
+ * @param priorKey - The prior search key, if any. Used for optimizations.
+ * @returns An array of matching item strings.
+ */
+export async function search(
+  key: string,
+  priorKey: string | null = null
+): Promise<string[]> {
+  let isPrefix = priorKey !== null && key.startsWith(priorKey);
+  return (await invoke("search", { key, isPrefix })) as string[];
+}
